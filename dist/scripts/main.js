c40b9b5c25e755afca5d6fb27a9b261967884a62
Syl <laracheramy@gmail.com>
2025-03-13 14:40:43 +0100

    class hidden speakers_details + update infos speakers 1/2

---
+++ dist/scripts/main.js
@@ -0,0 +1,5 @@
+const $speaker_main = document.querySelector(".speaker_main");
+const $speaker_details = document.querySelector(".speaker_details");
+const $btn_infos = document.querySelector(".info"); //afficher et click dessus re-toggle class
+const $btn_about = document.querySelector(".speaker_details__footer_btn"); // pour fermer les infos
+//add event listener et toggle class : hidden et active (les créer)
