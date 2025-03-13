4f68d61adb4d5f3e291b30fb8694b89e042d04a1
Syl <laracheramy@gmail.com>
2025-03-13 15:41:20 +0100

    gradient img speaker : TO DO : add div .gradient - + tests JS toggle class ... does not work :'(

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -3,3 +3,16 @@ const $speaker_details = document.querySelector(".speaker_details");
 const $btn_infos = document.querySelector(".info"); //afficher et click dessus re-toggle class
 const $btn_about = document.querySelector(".speaker_details__footer_btn"); // pour fermer les infos
 //add event listener et toggle class : hidden et active (les créer)
+
+$btn_infos.addEventListener("click", (e) => {
+  e.preventDefault();
+  $speaker_details.classList.toggle("hidden");
+  $speaker_main.classList.toggle("hidden");
+});
+
+$speaker_details.addEventListener("click", (e) => {
+  e.preventDefault();
+  $speaker_main.classList.toggle("hidden");
+  $speaker_details.classList.toggle("hidden");
+});
+//rien ne fonctionne c'est tristoune, c'est pacre que j'écris mal mon JS ou bien parce que la class hidden correpond à un display: none; ?
