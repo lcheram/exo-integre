242ddd872a8d64127c758c2a768bfcba48f64876
Syl <laracheramy@gmail.com>
2025-03-14 11:02:36 +0100

    nav icons cliquables, nav mobile hidden, added class info on btn speaker, form textarea height and min font size accessibility

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -15,4 +15,14 @@ $speaker_details.addEventListener("click", (e) => {
   $speaker_main.classList.toggle("hidden");
   $speaker_details.classList.toggle("hidden");
 });
-//rien ne fonctionne c'est tristoune, c'est pacre que j'écris mal mon JS ou bien parce que la class hidden correpond à un display: none; ?
+// faut que je boucle sur mes speakers, sinon ça prend que le premier. De toutes façons il y a un soucis avec mon display none qui ne prend pas sur un event. faudrait cibler le grand-parent de mon élément ? et mettre la classe info sur mes boutons partout dans mon HTML et pas juste sur mes deux premiers orateurs
+
+const $menu_mobile = document.querySelector(".mobile_nav__icons--menu");
+const $overlay = document.querySelector(".mobile_nav__overlay");
+
+$menu_mobile.addEventListener("click", (e) => {
+  e.preventDefault();
+  // ça ne donne rien de nouveau, faut vraiment que je fasse des exercices de JS, parce que je me souviens de rien
+  $overlay.classList.toggle("hidden");
+  console.log(e); // y a rien dans mon console log <3 because j'ai fait de la merde <3
+});
